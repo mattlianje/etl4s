11583095f02cbcb5f9af7569a388fbeede0e0e39
Matthieu Court <matthieu.court@instacart.com>
2025-10-14 00:27:57 -0400

    change publish and tweak doc pic + README

--- project.scala
+++ project.scala
@@ -7,10 +7,10 @@
 
 //> using publish.name "etl4s"
 //> using publish.organization "xyz.matthieucourt"
-//> using publish.version "1.4.1"
+//> using publish.version "1.5.0"
 //> using publish.url "https://github.com/mattlianje/etl4s"
 //> using publish.vcs "github:mattlianje/etl4s"
 //> using publish.license "Apache-2.0"
 
 //> using publish.developer "mattlianje|Matthieu Court|https://github.com/mattlianje"
-//> using publish.gpgKey "EBD031A099D734786A3D035A58C96F93A67A96E9"
+//> using publish.gpgKey "7CCC3BED8373C09380A4EEF5E610BC543D796B05"
